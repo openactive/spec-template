33a749b0bbb6b234105c579f6f266cafda5bfefd
Nick Evans <nick@imin.co>
2016-04-11 01:50:25 +0100

    First version of spec

--- deploy.sh
+++ deploy.sh
@@ -36,6 +36,10 @@ mkdir out;
 cd out
 git init
 
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "travis@openactive.org"
+
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
